0605890bb4b179d665d914f7c89d8c29f2701b80
Link <bal2457@utexas.edu>
2022-04-04 10:59:50 -0500

    updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_LafINjLjR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '15th6l8c0l529khcs93vka6ff2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
